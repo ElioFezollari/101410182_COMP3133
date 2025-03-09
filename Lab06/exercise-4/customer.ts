10494328785cd98d15e2d16cc9452b8595bf2e6a
ElioFezollari <fezollarielio@gmail.com>
2025-03-09 03:25:23 -0400

    Added Lab06

---
+++ Lab06/exercise-4/customer.ts
@@ -0,0 +1,13 @@
+export class Customer{
+    private firstName: string;
+    private lastName:string;
+
+    public greeter(){
+        console.log(`Hello ${this.firstName} ${this.lastName}`)
+    }
+
+    constructor(firstName: string, lastName: string){
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+}
---
+++ Lab06/exercise-4/main.ts
@@ -0,0 +1,4 @@
+import { Customer } from "./customer";
+
+let customer = new Customer("Elio","Fezollari");
+customer.greeter()
